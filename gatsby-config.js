a6a919ffc43bfb78a6dee460a656eed8ca10fe4d
asakuchi <paleapricot.asakuchi@gmail.com>
2020-12-05 13:26:40 +0900

    mod my-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/my-image.jpg`,
+        icon: `content/assets/my-image.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
