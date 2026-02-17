ec1107d7cdd9bdcfe0ff47a8d2a20d62f720bc83
asakuchi <49332371+asakuchi@users.noreply.github.com>
2026-02-18 03:16:33 +0900

    fix: 日付の表示形式を'MMMM DD, YYYY'から'YYYY/MM/DD'に変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -105,7 +105,7 @@ export const pageQuery = graphql`
           slug
         }
         frontmatter {
-          date(formatString: "MMMM DD, YYYY")
+          date(formatString: "YYYY/MM/DD")
           title
           description
           tags
